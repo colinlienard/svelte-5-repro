eb0bf68615b09373e14ec5d8f290dceedd4179ab
Colin Lienard <colin.lienard87@gmail.com>
2024-09-16 20:00:54 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { Action } from "svelte/action";
+
+  const action: Action<HTMLElement, { foo: string }> = (node, { foo }) => {
+    console.log(node, foo);
+  };
+</script>
+
+<div use:action={{ foo: "bar" }}></div>
